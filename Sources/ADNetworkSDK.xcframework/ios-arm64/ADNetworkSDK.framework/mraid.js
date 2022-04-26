d3fe90b56ae37aa884ccef1d89c95735550a82ea
Александр Троицкий <alexander.troitsky@gmail.com>
2022-04-26 22:12:57 +0300

    SDK framework with mraid

--- Sources/ADNetworkSDK.xcframework/ios-arm64/ADNetworkSDK.framework/mraid.js
+++ Sources/ADNetworkSDK.xcframework/ios-arm64/ADNetworkSDK.framework/mraid.js
@@ -6,20 +6,49 @@
         DEFAULT: 'default',
         HIDDEN: 'hidden'
      };
+     
+     var EVENTS = mraid.EVENTS = {
+         ERROR: 'error',
+         INFO: 'info',
+         READY: 'ready',
+         STATECHANGE: 'stateChange',
+         VIEWABLECHANGE: 'viewableChange',
+         EXPOSURECHANGE: "exposureChange",
+         SIZECHANGE: 'sizeChange'
+       };
+     
+     var PLACEMENTS = mraid.PLACEMENTS = {
+         INLINE : "inline",
+         INTERSTITIAL : "interstitial"
+     };
+     
+     var FEATURES = mraid.FEATURES = {
+         "SMS" : "sms",
+         "TEL" : "tel",
+         "STORE_PICTURE" : "storePicture",
+         "INLINE_VIDEO" : "inlineVideo",
+         "CALENDAR" : "calendar"
+     };
 
      var version = '3.0';
      var state = STATES.LOADING;
-     
+     var placementType = PLACEMENTS.INTERSTITIAL;
+     var listeners = [];
      var screenSize = { width:0, height:0 };
+     var supportedFeatures = [];
      mraid.isViewable = true;
 
      mraid.getVersion = function() {
-         //console.log('getVersion (version = ' + version + ")");
+         console.log('getVersion (version = ' + version + ")");
          return version;
      };
      
+     // ------------------------------------------------------------------------------
+     //                      State
+     // ------------------------------------------------------------------------------
+     
      mraid.getState = function(){
-         //console.log('getState (state = ' + state + ")");
+         console.log('getState (state = ' + state + ")");
          return state;
     }
      
@@ -27,12 +56,13 @@
          console.log("setState (" + toState + ")" );
          if(state != toState){
              state = toState;
-             document.dispatchEvent(new CustomEvent("mraidEvent", {bubbles: true,detail:{type: "stateChange"}}));
-             if (state === STATES.DEFAULT) {
-                 document.dispatchEvent(new CustomEvent("mraidEvent", {bubbles: true,detail:{type: "ready"}}));
-             }
+             mraid.fireEvent(EVENTS.STATECHANGE)
          }
     }
+     
+     // ------------------------------------------------------------------------------
+     //                      Screen size
+     // ------------------------------------------------------------------------------
      
      mraid.getScreenSize = function(){
          return screenSize;
@@ -41,16 +71,114 @@
      mraid.setScreenSize = function(width,height){
          screenSize.width = width;
          screenSize.height = height;
-         //console.log("setScreenSize: " + "w: " + width + ", h: " + height);
-         document.dispatchEvent(new CustomEvent("mraidEvent", {bubbles: true,detail:{type: "sizeChange"}}));
+         console.log("setScreenSize: " + "w: " + width + ", h: " + height);
+         mraid.fireEvent(EVENTS.SIZECHANGE)
      }
      
      mraid.setExposure = function(viewable){
          mraid.isViewable = viewable;
-         //console.log("setViewable: " + viewable);
-         document.dispatchEvent(new CustomEvent("mraidEvent", {bubbles: true,detail:{type: "exposureChange"}}));
+         console.log("setExposure: " + viewable);
+         mraid.fireEvent(EVENTS.EXPOSURECHANGE)
+     }
+     
+     // ------------------------------------------------------------------------------
+     //                      Placement
+     // ------------------------------------------------------------------------------
+     
+     mraid.getPlacementType = function(){
+         console.log('getPlacementType');
+         return placementType;
+     }
+     
+     mraid.setPlacementType = function(type){
+        if(type == PLACEMENTS.INLINE || type == PLACEMENTS.INTERSTITIAL) {
+            console.log('setPlacementType: ' + type);
+            placementType = type;
+        }
+     }
+     
+     // ------------------------------------------------------------------------------
+     //                      Support for features
+     // ------------------------------------------------------------------------------
+     mraid.setSupports = function(feature, isSupported){
+         console.log("set feature " + feature + " " + isSupported);
+         supportedFeatures[feature] = isSupported;
+     }
+     
+     mraid.supports = function(feature){
+         console.log('supports - ' + feature + ' : ' + (supportedFeatures[feature] === true));
+         return supportedFeatures[feature]=== true;
+     }
+     
+     // ------------------------------------------------------------------------------
+     //                      Event Listeners
+     // ------------------------------------------------------------------------------
+     
+     mraid.fireEvent = function(event){
+         console.log("fire event (" + event + ")");
+         if(listeners.containsEvent(event)){
+             listeners.invoke(event);
+         }
+     }
+     
+     mraid.addEventListener = function(event, listener){
+         if(listeners.containsListener(event, listener)){
+             console.log('addEventListener - this function already registered for (' + event + ') event.');
+             return;
+         }
+         console.log('addEventListener (event = ' + event + ')');
+         listeners[event] = listeners[event] || [];
+         listeners[event].push(listener);
      }
 
+     mraid.removeEventListener = function(event, listener){
+         listeners.removeListener(event, listener);
+     }
+     
+     
+     listeners.invoke = function(event){
+         listeners[event].forEach(function(listener){
+             listener();
+         });
+     }
+
+     listeners.containsEvent = function(event){
+         return Array.isArray(listeners[event]);
+     }
+
+     listeners.containsListener = function(event, listener){
+         var listenerString = String(listener);
+         if(Array.isArray(listeners[event])){
+             listeners[event].forEach(function(listener){
+                 var thisStr = String(listener);
+                 if(thisStr === listenerString){
+                     return true;
+                 }
+             });
+         }
+         return false;
+     }
+
+     listeners.removeListener = function(event, listener){
+         var listenerString = String(listener);
+         var index = -1;
+         if(Array.isArray(listeners[event])){
+             listeners[event].forEach(function(listener){
+                 var thisStr = String(listener);
+                 if(thisStr === listenerString){
+                     console.log("removing event listener (" + event + ")");
+                     index = listeners[event].indexOf(listener);
+                 }
+             });
+         }
+         if(index > 0){
+             listeners[event].splice(index, 1);
+         }
+     }
+
+    // ------------------------------------------------------------------------------
+    //                      Actions from UI
+    // ------------------------------------------------------------------------------
      
      mraid.close = function() {
          mraid.setState(STATES.HIDDEN);
