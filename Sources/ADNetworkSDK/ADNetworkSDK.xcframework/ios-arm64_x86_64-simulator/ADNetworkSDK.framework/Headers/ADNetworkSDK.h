a030ac9f352492e17e7637f896256f95d72eb51f
Александр Троицкий <alexander.troitsky@gmail.com>
2023-02-16 17:08:25 +0300

    changed dir structure

---
+++ Sources/ADNetworkSDK/ADNetworkSDK.xcframework/ios-arm64_x86_64-simulator/ADNetworkSDK.framework/Headers/ADNetworkSDK.h
@@ -0,0 +1,18 @@
+//
+//  ADNetworkSDK.h
+//  ADNetworkSDK
+//
+//  Created by Alexander Troitsky on 03.02.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ADNetworkSDK.
+FOUNDATION_EXPORT double ADNetworkSDKVersionNumber;
+
+//! Project version string for ADNetworkSDK.
+FOUNDATION_EXPORT const unsigned char ADNetworkSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ADNetworkSDK/PublicHeader.h>
+
+
